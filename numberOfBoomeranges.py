b18f1dfbc3b2c899afe275bb5139c1bee5b3d42f
klgentle <klgentle@sina.com>
2019-03-19 12:30:19 +0800

    net answer from comments

--- numberOfBoomeranges.py
+++ numberOfBoomeranges.py
@@ -3,36 +3,43 @@ logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - 
 import math
 #value = math.factorial(x)
 from pprint import pprint
-class Solution:
-    def distanceMeasure(self,list_a, list_b):
-        # list_a, list_b like [1,0], [2,3] pairwise
-        # 距离相等? which distance #math.sqrt
-        dist = (list_a[0]-list_b[0])**2 + (list_a[1]-list_b[1])**2
-        logging.debug('dist: %s' % dist)
-        return dist
-    def numberOfBoomerangs(self, points) -> int: # : List[List[int]]
-        # points likes [[0,0],[1,0],[2,0]]
-        points_len = len(points)
-        tmp_list = []
-        value_dict = {}
-        ret = 0
-        for i in range(0,points_len):
-            logging.debug('points[%s]: %s' % (i,points[i]))
-            print('value_dict: ',value_dict)
-            print('tmp_list: ',tmp_list)    
-            for j in range(0,points_len):
-                if i == j :
+
+class Solution(object):
+    def numberOfBoomerangs(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        2 6 12    
+        """
+        def calDis(p1, p2):
+            l1 = p1[0] - p2[0]
+            l2 = p1[1] - p2[1]
+            return l1 * l1 + l2 * l2
+
+        l = len(points)
+        if l < 3:
+            return 0
+        
+        cnt = 0
+        
+        for i in range(l):
+            distance = dict()
+            for j in range(l):
+                if j == i:
                     continue
-                dist = self.distanceMeasure(points[i],points[j])
-                if dist not in tmp_list:
-                    tmp_list.append(dist)
-                else:
-                    if dist not in value_dict:
-                        value_dict[dist] = 2
+                
+                dis = calDis(points[i], points[j])
+                
+                if dis in distance:
+                    distance[dis] += 1
+                    num = distance[dis]
+                    
+                    if num == 2:
+                        cnt += 2
                     else:
-                        value_dict[dist] += 1
-        for k,v in value_dict.items():
-            print('v:', v)
-            ret += v*(v-1)
-        print('ret:', ret)
-        return ret
+                        cnt += (num-1) * 2
+                     
+                else:
+                    distance[dis] = 1
+                    
+        return cnt
