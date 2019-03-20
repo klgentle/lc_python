d95c3de032ef4d5efa7c7f3883fdde454975c244
klgentle <klgentle@sina.com>
2019-03-20 21:01:11 +0800

    20190320

--- findErrorNums.py
+++ findErrorNums.py
@@ -7,18 +7,23 @@ class Solution:
         for i in set_all - set(nums):
             the_lost.append(i)
             break
-        for i in nums:
+        while len(nums) > 0:
+            i = nums[0]
+            print('i: ', i)
+            if i == 4:
+                print('i==4_________________')
+            nums.remove(i)
             try:
-                nums.remove(i)
                 nums.remove(i)
             except ValueError:
                 pass
             else:
-                the_dulp.append(i) # is this not work? when?
+                the_dulp.append(i)
+        print(the_dulp+the_lost)
         return the_dulp+the_lost
 
-if '__name__' = '__main__':
+if __name__ == '__main__':
     a = Solution()
     l = [33,86,39,20,22,99,75,1,31,58,35,13,48,66,80,82,94,14,50,93,43,63,98,95,8,70,44,68,74,17,59,36,5,23,7,69,3,21,30,92,78,73,77,54,47,42,40,34,64,11,51,4,57,15,16,28,12,29,62,4,91,18,83,45,38,56,2,84,27,6,41,61,88,52,71,90,67,79,76,24,37,96,19,97,53,26,87,49,9,85,32,72,10,89,55,46,81,65,60]
     a.findErrorNums(l)
-    # questions: why the dulpicate 4 can not find? 
+    # questions: why the dulpicate 4 can not find? when remove in for, for maynot work
