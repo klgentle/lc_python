0ebdd7403fbaf43e47d142e1b57d73e0d73ab2e0
klgentle <klentle@sina.com>
2019-03-24 21:29:59 +0800

    leet_code python3 20190324 easy_case

---
+++ easy_case/isValid_answer.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        if len(s) % 2 != 0:
+            return False
+
+        lb = {'(':')','[':']','{':'}'}
+        queue = []
+        for x in s:
+            if x in lb:
+                queue.append(x)
+            else:
+                if len(queue) == 0:
+                    return False
+                top = queue.pop()
+                if lb[top] != x:
+                    return False
+
+        return len(queue) == 0
---
+++ easy_case/longestCommonPrefix_answ.py
@@ -0,0 +1,25 @@
+class Solution:
+    def longestCommonPrefix(self, strs: 'List[str]') -> 'str':
+        if len(strs) == 0:
+            return ''
+        if len(strs) == 1:
+            return strs[0]
+
+        prefix, chars = '', zip(*strs)
+        print('zip(*strs):', chars)
+        print('enumerate(chars):', enumerate(chars))
+        for i, group in enumerate(chars):
+            ch = group[0]
+            for j in range(1, len(group)):
+                if group[j] != ch:
+                    return prefix
+            print('prefix:', prefix)
+            prefix += strs[0][i]
+        return prefix
+
+if __name__ == '__main__':
+    a = Solution()
+    s = ["flower","flow","flight"] 
+    s1= ['a']
+    s2 = ["aaa","aa","aaa"]
+    a.longestCommonPrefix(s)
