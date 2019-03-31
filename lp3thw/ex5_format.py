65f721a777f642dbbdb1e8dc6dc2275a24a9b78d
klgentle <klentle@sina.com>
2019-03-31 10:10:14 +0800

    ex5 format

--- lp3thw/ex5_format.py
+++ lp3thw/ex5_format.py
@@ -2,7 +2,7 @@ my_name = 'Zed A. Shaw'
 my_age = 35 # not a lie
 my_height = 170 # cm
 
-#format string
+# f"" format string
 print(f"let's talk about {my_name}")
 print(f"He's {my_height} inches tall.")
 print(f"He get {my_age}")
