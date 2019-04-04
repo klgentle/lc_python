ef43a93b906c380af277efea3d2d12d4849d25ed
klgentle <klgentle@sina.com>
2019-04-04 15:58:53 +0800

    20190403 add

---
+++ lp3thw/ex43_dedent.py
@@ -0,0 +1,13 @@
+from sys import exit
+from random import randint
+from textwrap import dedent
+
+
+
+# test dedent
+print("""
+        From here without dedent
+        """)
+print(dedent("""
+        From here with dedent
+        """))
