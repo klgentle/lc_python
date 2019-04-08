3fbb094972362c57164f5a9a05fdcdf464cbdf46
klgentle <klgentle@sina.com>
2019-04-08 08:50:57 +0800

    20190408

--- lp3thw/ex43_dedent.py
+++ lp3thw/ex43_dedent.py
@@ -2,12 +2,34 @@ from sys import exit
 from random import randint
 from textwrap import dedent
 
+class Scene(object):
+
+    def enter(self):
+        print("This scene is not yet configured.")
+        print("Subclass it and implement enter().")
+
+
+
+class Engine(object):
+    def __init__(self, scene_map):
+        self.scene_map = scene_map 
+        
+    def play(self):
+        current_scene = self.scene_map.opening_scene()
+        last_scene = self.scene_map.next_scene('finished')
+        while current_scene != last_scene:
+            next_scene_name = current_scene.enter()
+            current_scene = self.scene_map.next_scene(next_scene_name)
+
+        # be sure to print out the last scene
+        current_scene.enter()
+
 
 
 # test dedent
-print("""
-        From here without dedent
-        """)
-print(dedent("""
-        From here with dedent
-        """))
+#print("""
+#        From here without dedent
+#        """)
+#print(dedent("""
+#        From here with dedent
+#        """))
