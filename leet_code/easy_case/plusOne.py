afafee9332c08b59890e2f2c756052b1ff884de6
klgentle <klentle@sina.com>
2019-04-14 14:23:49 +0800

    2190414 leetcode

--- leet_code/easy_case/plusOne.py
+++ leet_code/easy_case/plusOne.py
@@ -1,14 +1,18 @@
 class Solution:
     def plusOne(self, digits: list) -> list:
         l = len(digits)
-        if digits[-1] == 9:
+        if digits[-1] < 9:
+            digits[-1] += 1
+        elif digits[-1] == 9:
             digits[-1] = 10
-        for i in range(-1, -l, -1):
-            print(i)
-            if digits[i] == 10:
-                digits[i] = 0
-                digits[i - 1] += 1
-        print(f"digits:{digits}")
+            for i in range(-1, -l, -1):
+                #print(i)
+                if digits[i] == 10:
+                    digits[i] = 0
+                    digits[i - 1] += 1
+            if digits[0] == 10:
+                digits = [1, 0] + digits[1:]
+            print(f"digits:{digits}")
         return digits
 
 
