7f8d1f35e93f93deabf1583d5e4e640c0903fec5
klgentle <klgentle@sina.com>
2019-04-18 17:58:16 +0800

    copy_upload.py complete

--- vs_code/copy_upload.py
+++ vs_code/copy_upload.py
@@ -8,8 +8,8 @@ import csv
 
 def copyRegister(date_str:str):
     """ copy register """ 
-    dir_name = "E:\\svn\\1300_编码\\发布登记表"
-    work_tmp = "E:\\yx_walk\\common_sense\\yx_python\\tmp"
+    code_home = "E:\\svn"
+    dir_name = os.path.join(code_home, "1300_编码\\发布登记表")
     code_bate_path = "E:\\yx_walk\\report_develop\\sky"
     target_path = os.path.join(code_bate_path, date_str+'bate')
     os.makedirs(target_path, exist_ok=True)
@@ -22,28 +22,39 @@ def copyRegister(date_str:str):
         for filename in filenames:
             #print('FILE INSIDE ' + folderName + ': '+ filename)
             if filename.find(date_str) > -1:
-                print(f"today filename: {filename} --------------------------") 
+                print(f"filename: {filename} --------------------------") 
                 whole_filename = os.path.join(folderName,filename) 
-                # copy excl files
-                #shutil.copy(whole_filename, work_tmp)
 
                 # copy excel file content
                 wb = openpyxl.load_workbook(whole_filename)
-                sheet = wb.get_sheet_by_name('全量报表存储过程')
+                sheet = wb.active
                 countyData = {}
-                print('Reading rows...')
                 for row in range(2, sheet.max_row + 1):
-                # Each row in the spreadsheet has data for one census tract.
                     name = sheet['C' + str(row)].value
                     if not name:
                         continue
                     file_type = sheet['D' + str(row)].value
                     path = sheet['E' + str(row)].value
                     data_list = [sheet[chr(i+ord('A')) + str(row)].value for i in range(0,10)]
-                    print(f'date_list:{data_list}')
+                    #print(f'date_list:{data_list}')
                     csvWriter.writerow(data_list)
+                    ind = path.find('1300_编码')
+                    if ind > -1:
+                        new_path = os.path.join(code_home,path[ind:])
+                        new_file = os.path.join(code_home,path[ind:],name+'.'+file_type.lower())
+                        #print(f"new_file:{new_file}")
+
+                        targetName = path[ind:].split('\\')[1].split('_')[1]
+                        #print(f"targetName:{targetName}")
+                        target_path2 = os.path.join(target_path,targetName) 
+                        os.makedirs(target_path2, exist_ok=True)
+                        try:
+                            shutil.copy(new_file, target_path2)
+                        except FileNotFoundError:
+                            print(f"error! No such file: {new_file}")
+                    
     csvFile.close()
 
 
 if __name__ == '__main__':
-    copyRegister('20190418')
+    copyRegister('20190417')
