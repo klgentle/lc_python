770f59a08c69724dae9560204b2ba710218e28e9
klgentle <klgentle@sina.com>
2019-05-04 13:04:54 +0800

    20190503

--- leet_code/easy_case/rob_ans2.py
+++ leet_code/easy_case/rob_ans2.py
@@ -4,16 +4,17 @@ import pysnooper
 
 class Solution(object):
     # debug here
-    #@pysnooper.snoop('/home/kl/log/leet_code/1.log')
     @pysnooper.snoop()
     def rob(self, nums: list) -> int:
         """
         :type nums: List[int]
+        # 这是一个动态规划问题
         """
+         
+
         last = 0
         now = 0
         for i in nums:
-            # 这是一个动态规划问题
             last, now = now, max(last + i, now)
         return now
 
