2584461a1b1f99cedabc887f11590c2b07d308f9
klgentle <klgentle@sina.com>
2019-08-18 16:07:26 +0800

    complecate

--- base_practice/stock_anal.py
+++ base_practice/stock_anal.py
@@ -10,21 +10,25 @@ import pandas as pd
 from_date = "2018/1/17"
 to_date = "2019/8/16"
 
-start = "2018-03-20" 
-#start = input("Enter the beginning date between 2018-1-17 and 2019-8-16: ")
+# for test start = "2018-03-20"
+start = input("Enter the beginning date between 2018-1-17 and 2019-8-16: ")
 start_list = start.split("-")
 # date format
-start_date = start_list[0] + "/" + str(int(start_list[1])) + "/" + str(int(start_list[2]))
+start_date = (
+    start_list[0] + "/" + str(int(start_list[1])) + "/" + str(int(start_list[2]))
+)
 # print(start_date)
 while start_date < from_date or start_date > to_date:
     print("Start date is out of the date range")
     start = input("Enter the beginning date between 2018-1-17 and 2019-8-16: ")
     start_list = start.split("-")
     # date format
-    start_date = start_list[0] + "/" + str(int(start_list[1])) + "/" + str(int(start_list[2]))
+    start_date = (
+        start_list[0] + "/" + str(int(start_list[1])) + "/" + str(int(start_list[2]))
+    )
 
-end = '2018-08-16'
-#end = input("Enter the ending date between 2018-1-17 and 2019-8-16: ")
+# for test end = '2018-08-16'
+end = input("Enter the ending date between 2018-1-17 and 2019-8-16: ")
 end_list = end.split("-")
 # date format
 end_date = end_list[0] + "/" + str(int(end_list[1])) + "/" + str(int(end_list[2]))
@@ -44,31 +48,38 @@ print("Summary of stock prices from", start, "to", end)
 CMG = pd.read_csv("CMG.csv")
 COKE = pd.read_csv("COKE.csv")
 NKE = pd.read_csv("NKE.csv")
-# add Symbol for stock
-length = len(CMG['Date'])
-CMG['Symbol'] = ['CMG'] * length
-COKE['Symbol'] = ['COKE'] * length
-NKE['Symbol'] = ['NKE'] * length
+# add Symbol for stock to mark
+length = len(CMG["Date"])
+CMG["Symbol"] = ["CMG"] * length
+COKE["Symbol"] = ["COKE"] * length
+NKE["Symbol"] = ["NKE"] * length
 
 all_data = pd.concat([CMG, COKE, NKE])
-all_data['Date'] = pd.to_datetime(all_data['Date']) #将数据类型转换为日期类型
-all_data = all_data.set_index('Date') # 将date设置为index
-#print(all_data.head())
-select=all_data.loc[start:end,['Adj Close', 'Symbol']]
+all_data["Date"] = pd.to_datetime(all_data["Date"])  # 将数据类型转换为日期类型
+all_data = all_data.set_index("Date")  # 将date设置为index
+select = all_data.loc[start:end, ["Adj Close", "Symbol"]]
 
-print(select['Adj Close'].groupby(select['Symbol']).mean().head())
+# rename column for agg
+select = select.rename({"Adj Close": "Adj_Close"}, axis="columns")
 
-df1 = pd.DataFrame(select.groupby(select['Symbol']).mean())
+# get the begin data
+begining_price = select.loc[start:start, ["Adj_Close", "Symbol"]]
+# get the end data
+ending_price = select.loc[end:end, ["Adj_Close", "Symbol"]]
+# transfer into group by for join
+begin_group = begining_price.groupby("Symbol").max()
+end_group = ending_price.groupby("Symbol").max()
 
-#df2 = pd.DataFrame(select.groupby(select['Symbol']).min().reset_index(),  columns=['Minimum'])   #'Symbol',
-df2 = pd.DataFrame(select.groupby(select['Symbol']).min())
-print("df2",df2)
-df3 = pd.DataFrame(select.groupby(select['Symbol']).max())
-print("df3",df3)
-df_out = pd.merge(df1,df2,df3)
+# rename column for display
+begin_group = begin_group.rename({"Adj_Close": "Begining Price"}, axis="columns")
+end_group = end_group.rename({"Adj_Close": "Ending Price"}, axis="columns")
 
-print("merge_df",merge_df)
-
-#print(loan50['Cust_ID'].groupby(loan50['OUTCOME']).count(),'\n\n\n')
-#print(loan50.groupby(loan50['OUTCOME']).mean())
-
+# data format for display
+pd.options.display.float_format = "{:,.2f}".format
+stock_out = pd.DataFrame(select.groupby("Symbol").Adj_Close.agg(["min", "max", "mean"]))
+# rename column for display
+stock_out = stock_out.rename(
+    {"min": "Minimum", "max": "Maximum", "mean": "Average"}, axis="columns"
+)
+stock_out = pd.concat([begin_group, end_group, stock_out], axis="columns")
+print(stock_out)
