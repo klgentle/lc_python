5382ea628f7ea7b07274f6ee8bea4a01eb759152
klgentle <klgentle@sina.com>
2019-09-01 22:54:01 +0800

    update

--- leet_code/hard_case/0042_Trapping_Rain_Water3.py
+++ leet_code/hard_case/0042_Trapping_Rain_Water3.py
@@ -1,5 +1,4 @@
 class Solution:
-    # danamic use to cycle
     def trap(self, height: List[int]) -> int:
         if not height:
             return 0
@@ -8,15 +7,13 @@ class Solution:
         height_len = len(height)
         left_max = [height[0]] * height_len
         right_max = [height[-1]] * height_len
-        water = [0] * height_len
         
         for i in range(1, height_len):
             left_max[i] = max(left_max[i-1], height[i])
             
-        for i in range(height_len-2, -1, -1):
-            # from max to min, init max index first, use max index, not min
-            right_max[i] = max(right_max[i+1], height[i])
-            water[i] = min(right_max[i], left_max[i]) - height[i]
+        for i in range(height_len -2, -1, -1):
+            right_max[i] = max(right_max[i+1], height[i]) # init large index first
+            sum_water += min(left_max[i], right_max[i]) - height[i]
             
-        water[-1] = min(right_max[-1], left_max[-1]) - height[-1]
-        return sum(water)
+        sum_water += min(right_max[-1], right_max[-1]) - height[-1]
+        return sum_water
