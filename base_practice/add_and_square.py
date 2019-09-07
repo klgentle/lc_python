d971ccdfa2fc02c29d3b581c6e48c95afb566c26
klgentle <kegentle@sina.com>
2019-09-07 14:32:06 +0800

    add if name

--- base_practice/add_and_square.py
+++ base_practice/add_and_square.py
@@ -8,6 +8,8 @@ def add_and_square(number: int):
             if a**2 + b**2 == c**2:
                 print(f"a : {a}, b: {b}, c:{c}")
 
-start_time = time.time()
-add_and_square(1000)
-end_time = time.time()
+
+if __name__ == "__main__":
+    start_time = time.time()
+    add_and_square(1000)
+    end_time = time.time()
