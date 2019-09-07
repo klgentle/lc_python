0ed2328eacd0f004deb9a376d18fa87d2ad7b089
klgentle <kegentle@sina.com>
2019-09-07 12:25:53 +0800

    add for test

---
+++ base_practice/add_and_square.py
@@ -0,0 +1,13 @@
+import time
+
+
+def add_and_square(number: int):
+    for a in range(0, number):
+        for b in range(0, number):
+            c = number - a - b
+            if a**2 + b**2 == c**2:
+                print(f"a : {a}, b: {b}, c:{c}")
+
+start_time = time.time()
+add_and_square(1000)
+end_time = time.time()
