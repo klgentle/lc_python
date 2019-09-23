b3484e341b27e10f9be12fb026b9b06274c7ccad
klgentle <klgentle@sina.com>
2019-09-23 10:36:45 +0800

    db connect functional

--- database/cx_Oracle_test.py
+++ database/cx_Oracle_test.py
@@ -1,13 +1,18 @@
-import cx_Oracle
+from data_base_connect import connect
+#import cx_Oracle
 
 # use in linux or centos, not in windos (libclntsh.so: cannot open shared object file: No such file or directory)
-conn = cx_Oracle.connect('rptuser', 'rptuser','100.11.94.176:1521/odsdb')
+#conn = cx_Oracle.connect('rptuser', 'rptuser','100.11.94.176:1521/odsdb')
+conn = connect()
 cursor = conn.cursor()
 cursor.execute("select id from rpt_account_mid where data_date != date'2018-08-30'")
 
-result = cursor.fetchall()
-for row in result:
-    print(row[0])
+#result = cursor.fetchall()
+#for row in result:
+#    print(row[0])
+result = cursor.fetchone()
+print(result[0])
+
 
 cursor.close()
 conn.close()
---
+++ database/data_base_connect.py
@@ -0,0 +1,6 @@
+import cx_Oracle
+
+
+def connect(da_name="171"):
+    conn = cx_Oracle.connect('rptuser', 'rptuser','100.11.94.176:1521/odsdb')
+    return conn
