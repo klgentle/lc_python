bc516efb10384d28aa1e6a1acb5e78da2aa743e2
klgentle <klgentle@sina.com>
2019-09-26 22:49:38 +0800

    update add log

--- database/ProcedureLogModify.py
+++ database/ProcedureLogModify.py
@@ -33,26 +33,32 @@ class ProcedudreLogModify(object):
         return proc_cont.replace("BEGIN", job_step_variable + "BEGIN")
 
     @staticmethod
-    def set_job_step_value(proc_cont):
+    def set_job_step_value_and_modify_insert(proc_cont):
         """
         . 设置v_job_step的值
         . 将写死的job_step,改为变量
         """
-        error_bat_log_value = "VALUES(BAT_SERIAL_NO.NEXTVAL, V_DEAL_DATE,'-1',V_JOB_NAME"
+        error_bat_log_value = (
+            "VALUES(BAT_SERIAL_NO.NEXTVAL, V_DEAL_DATE,'-1',V_JOB_NAME"
+        )
         bat_log_value = "VALUES(BAT_SERIAL_NO.NEXTVAL, V_DEAL_DATE,"
         proc_cont_last_half = proc_cont.split(bat_log_value)[1]
         job_step = proc_cont_last_half.split(",")[0]
 
         if proc_cont.find(error_bat_log_value) > -1:
-            proc_cont = proc_cont.replace('-1', "V_JOB_STEP")
+            proc_cont = proc_cont.replace("-1", "V_JOB_STEP")
         else:
             # add line of v_job_step
             # VALUES(BAT_SERIAL_NO.NEXTVAL, V_DEAL_DATE,'19',V_JOB_NAME
             v_job_step_line = "V_JOB_STEP:={};\n".format(int(eval(job_step)))
-            proc_cont = proc_cont.replace("INSERT INTO", v_job_step_line + "INSERT INTO")
+            proc_cont = proc_cont.replace(
+                "INSERT INTO", v_job_step_line + "INSERT INTO"
+            )
 
             # replace hard code value with v_job_step
-            proc_cont = proc_cont.replace(bat_log_value + job_step, bat_log_value + "V_JOB_STEP")
+            proc_cont = proc_cont.replace(
+                bat_log_value + job_step, bat_log_value + "V_JOB_STEP"
+            )
         return proc_cont
 
     @staticmethod
@@ -65,18 +71,11 @@ class ProcedudreLogModify(object):
         """修改 bat_report_log 登记
         """
         print("修改 bat_report_log 登记")
-        proc_cont = self.set_job_step_value(proc_cont)
+        proc_cont = self.set_job_step_value_and_modify_insert(proc_cont)
         # 4.查找是否spend time已经减一了，如果有就修正
         proc_cont = self.spend_time_value_adjust(proc_cont)
 
         return proc_cont
-
-    # @staticmethod
-    # def add_report_log(proc_cont:str)-> str:
-    #    """添加 bat_report_log 登记
-    #    TODO两个timestamp之间不是很好的划分，
-    #    """
-    #    pass
 
     @staticmethod
     def add_header_log(proc_cont: str) -> str:
@@ -89,26 +88,65 @@ class ProcedudreLogModify(object):
         return proc_cont.replace(log_end, header_log + log_end)
 
     @staticmethod
-    def is_log_exists_between_timestamp(proc_cont_between_commmit) -> bool:
-        return proc_cont_between_commmit.find("BAT_REPORT_LOG") > -1
+    def is_log_exists_and_need_modify(proc_cont_between_log) -> bool:
+        proc_cont_list = proc_cont_between_log.split("VALUES(BAT_SERIAL_NO.NEXTVAL,")
+        # 如果存在VALUES(BAT_SERIAL_NO.NEXTVAL,则list长度会大于一
+        if len(proc_cont_list) > 1 and not proc_cont_list[1].strip().startswith(
+            "V_DEAL_DATE,V_JOB_STEP,V_JOB_NAME"
+        ):
+            return True
+        return False
 
     def modify_procedure_header(self, proc_cont: str) -> str:
         """修改存储过程头部部分
         """
         proc_cont = self.add_header_log(proc_cont)
-        # not find then add 
+        # not find then add
         if proc_cont.find("V_JOB_STEP") == -1:
             proc_cont = self.declare_job_step(proc_cont)
         return proc_cont
 
-    def modify_procedure_between_timestamp(self, proc_cont: str):
-        """修改存储过程两个timestamp之间的部分
-            TODO 如果两个;之间没有timestamp,会导致无法添加日志
+    @staticmethod
+    def bat_log_template():
+        bat_report_log = """
+COMMIT;
+
+V_END_TIME:=CURRENT_TIMESTAMP ;    --处理结束时间
+V_SPEND_TIME:=ROUND(TO_NUMBER(TO_DATE(TO_CHAR(V_END_TIME,'YYYY-MM-DD HH24:MI:SS') ,'YYYY-MM-DD HH24:MI:SS')
+ -TO_DATE(TO_CHAR(V_ST_TIME,'YYYY-MM-DD HH24:MI:SS') ,'YYYY-MM-DD HH24:MI:SS')) * 24 * 60 * 60);----执行时间
+
+V_JOB_STEP:=1;
+INSERT INTO BAT_REPORT_LOG(DEAL_SERIAL_NO,DEAL_DATE,JOB_STEP,JOB_NAME,SPEND_TIME,REMARK,JOB_ID,JOB_STATE)
+VALUES(BAT_SERIAL_NO.NEXTVAL, V_DEAL_DATE,V_JOB_STEP,V_JOB_NAME,V_SPEND_TIME,V_ERR_MSG,V_JOB_ID,'结束处理...');
+COMMIT;
         """
-        if self.is_log_exists_between_timestamp(proc_cont):
-            return self.modify_report_log(proc_cont)
-        # else:
-        #    return self.add_report_log(proc_cont)
+        return bat_report_log
+
+    @staticmethod
+    def add_report_log(content: str, target_str: str) -> str:
+        content_list = content.split(target_str)
+        # s2 =['d', ' b ', ' c ', ' css']
+        # 从第二个至最后都要加日志
+        # TODO 日志编号 编号可以在最后统一重写一遍
+        to_add_log_list = content_list[2:]
+        # the last one no need
+        log_content_list = [" "] * len(to_add_log_list)
+        for i in range(0, len(to_add_log_list) - 1):
+            sql = to_add_log_list[i] + self.bat_log_template()
+            log_content_list[i] = sql
+
+        log_content_list[-1] = to_add_log_list[-1]
+        return target_str.join(content_list[0:2] + log_content_list)
+
+    def modify_procedure_between_report_log(self, proc_cont: str):
+        """修改存储过程两个report_log之间的部分
+            TODO 如果两个;之间没有report_log,会导致无法添加日志
+        """
+        if self.is_log_exists_and_need_modify(proc_cont):
+            proc_cont = self.modify_report_log(proc_cont)
+        # TODO test
+        # if proc_cont.count('INSERT INTO') >2:
+        #    proc_cont =  self.add_report_log(proc_cont)
         return proc_cont
 
     def modify_procedure_log(self):
@@ -119,10 +157,10 @@ class ProcedudreLogModify(object):
         proc_cont_head, proc_cont_main = proc_cont.split("IF I_RUN_DATE IS NULL THEN\n")
         proc_cont_head = self.modify_procedure_header(proc_cont_head)
 
-        split_str = "CURRENT_TIMESTAMP"
+        split_str = "V_JOB_ID"
         proc_cont_main_list = proc_cont_main.split(split_str)
         for ind in range(0, len(proc_cont_main_list)):
-            proc_cont_main_list[ind] = self.modify_procedure_between_timestamp(
+            proc_cont_main_list[ind] = self.modify_procedure_between_report_log(
                 proc_cont_main_list[ind]
             )
 
