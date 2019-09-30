86d9744aba55bea437331c184bb6cf4f59c829bd
klgentle <klgentle@sina.com>
2019-09-30 15:49:30 +0800

    logic enhance

--- automatic_office/test_docx.py
+++ automatic_office/test_docx.py
@@ -4,7 +4,7 @@ import docx
 class TestDocx(unittest.TestCase):
     def test_para_run(self):
         doc = docx.Document('文思员工-8月签到表_新模版_带加班.docx')        
-        print(len(doc.paragraphas))
+        print(len(doc.paragraphs))
 
     
 
