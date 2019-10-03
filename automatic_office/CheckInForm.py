8929bff86cdc76b03ca3dca865e97f96419941c4
klgentle <klgentle@sina.com>
2019-10-03 09:29:55 +0800

    docx detail ajust complete

--- automatic_office/CheckInForm.py
+++ automatic_office/CheckInForm.py
@@ -96,7 +96,8 @@ class CheckInForm(object):
             hdr_cells[i].text = head_list[i]
             hdr_cells[i].paragraphs[0].alignment = WD_ALIGN_PARAGRAPH.CENTER  #水平居中
             hdr_cells[i].vertical_alignment = WD_ALIGN_VERTICAL.CENTER  # 垂直居中
-        date_str_list = []
+            hdr_cells[i].width = Cm(2.82)
+        date_list = []
         if form_type.lower() == "normal":
             date_list = self.get_all_work_date()
         elif form_type.lower() == "overtime":
@@ -106,6 +107,9 @@ class CheckInForm(object):
             row_cells = table.add_row().cells
             row_cells[0].text = str(ind+1)
             row_cells[1].text = date.strftime("%Y-%m-%d")
+            # TODO set font name
+            #font.name = u'Calibri'
+            #run._element.rPr.rFonts.set(qn('w:eastAsia'), u'Calibri')
             row_cells[2].text = ""
             row_cells[3].text = ""
             row_cells[4].text = ""
@@ -114,12 +118,13 @@ class CheckInForm(object):
             for i in range(len(head_list)):
                 row_cells[i].paragraphs[0].alignment = WD_ALIGN_PARAGRAPH.CENTER  #水平居中
                 row_cells[i].vertical_alignment = WD_ALIGN_VERTICAL.CENTER  # 垂直居中
-
+                row_cells[i].width = Cm(2.82)
         # 设置行高
-        if form_type.lower() == "normal":
-            for row in table.rows:
-                # WHY 1cm not the same?
-                row.height = Cm(0.88)  # 1CM
+        for row in table.rows:
+            if form_type.lower() == "normal":
+                row.height = Cm(0.88)
+            elif form_type.lower() == "overtime":
+                row.height = Cm(0.65)
         return table
 
     def write_form(self, form_type: str):
@@ -136,9 +141,12 @@ class CheckInForm(object):
         document.styles['Normal'].font.name = u'宋体'   #微软雅黑
         document.styles['Normal'].font.size = Pt(10.5)   
         document.styles['Normal']._element.rPr.rFonts.set(qn('w:eastAsia'), u'宋体')
+        # 页面布局
         sec = document.sections[0]             # sections对应文档中的“节”
         sec.top_margin = Cm(2.3)
         sec.bottom_margin = Cm(2.3)
+        sec.page_height = Cm(29.7)
+        sec.page_with = Cm(21)
 
         # add header
         paragraph = document.add_paragraph()
@@ -153,23 +161,27 @@ class CheckInForm(object):
         #paragraph_format.line_spacing = 1.5 # 1.5倍行间距
         #paragraph_format.space_after =Pt(0)       #设置段后间距
 
-        paragraph = document.add_paragraph('姓名： 			         日期：{} 至 {}'.format(
+        paragraph = document.add_paragraph('姓名： 			 日期：{} 至 {}'.format(
             self.__work_date_start, self.__work_date_end))
         paragraph.alignment = WD_ALIGN_PARAGRAPH.LEFT
         paragraph_format = paragraph.paragraph_format
-        #paragraph_format.first_line_indent = Inches(0.3) # 首行缩进
+        paragraph_format.first_line_indent = Inches(0.3) # 首行缩进
         paragraph_format.left_indent = Cm(3.7) # 左侧缩进3.7cm
         #paragraph_format.line_spacing = 1.5 # 1.5倍行间距
         paragraph_format.space_after =Pt(0)       #设置段后间距
 
         table = self.check_in_add_table(document, form_type)
         table.alignment = WD_TABLE_ALIGNMENT.CENTER
-        document.save(r'automatic_office\{0}文思员工-签到表_{1}.docx'.format(
-            self.__year_month, form_type_name_dict.get(form_type)))
+        document.save(r'.\doc_file\文思员工-{0}月签到表_新模版_{1}.docx'.format(
+            self.__input_month, form_type_name_dict.get(form_type)))
         logging.info("Done!")
 
 
 if __name__ == "__main__":
-    obj = CheckInForm("201908")
+    if len(sys.argv) <= 1:
+        logging.info("Please input year_moth(YYYYMM)")
+        sys.exit(1)
+
+    obj = CheckInForm(sys.argv[1])
     obj.write_form('overtime')
     obj.write_form('normal')
