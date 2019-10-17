3896f4b38a1501aa4ac015969466c26048222050
klgentle <klgentle@sina.com>
2019-10-17 20:46:34 +0800

    rename scrapy

---
+++ scrapy_test/ScrapyBoForUpdate.py
@@ -0,0 +1,9 @@
+"""
+TODO 写一个爬虫，从服务器取最新Bo,然后保存到本地
+"""
+class ScrapyBoForUpdate(object):
+    def __init__(self):
+        self.bo_url = r"http://100.11.94.171:8080/BOE/BI"
+
+    def login(self):
+        pass
