5fef1ade2f5327d91bba7bfdae0f535fb780a420
klgentle <klgentle@sina.com>
2019-10-21 17:07:42 +0800

    try catch exception, and log Exception.__doc__

--- database/batch_replace.py
+++ database/batch_replace.py
@@ -33,7 +33,7 @@ def read_file_to_list(file_name: str) -> list:
 
 
 def batch_replace(proc_list: list, replace_type="view_and_log"):
-    error_list = []
+    error_dict = {}
     for proc in proc_list:
         # modify view name, data_area
         try:
@@ -46,11 +46,12 @@ def batch_replace(proc_list: list, replace_type="view_and_log"):
             else:
                 run_replace_view(proc)
                 run_modify_log(proc)
-        except:
-            error_list.append(proc)
+        except Exception as e:
+            # error_dict[proc] = e.__str__
+            error_dict[proc] = e.__doc__
             print(f"error, {proc}")
 
-    return error_list
+    return error_dict
 
 
 if __name__ == "__main__":
@@ -60,10 +61,32 @@ if __name__ == "__main__":
 
     # file_name = r"database\payment_list.txt"
     # file_name = r"E:\yx_walk\report_develop\view_to_replace\deposit_report_list.txt"
-    file_name = r"E:\yx_walk\report_develop\view_to_replace\cif_list.txt"
-    # file_name = r"E:\yx_walk\report_develop\view_to_replace\bi_list.txt"
-    proc_list = read_file_to_list(file_name)
-    proc_list = ["p_rpt_" + x.lower() for x in proc_list]
+    # file_name = r"E:\yx_walk\report_develop\view_to_replace\cif_list.txt"
+    ## file_name = r"E:\yx_walk\report_develop\view_to_replace\bi_list.txt"
+    # proc_list = read_file_to_list(file_name)
+    # proc_list = ["p_rpt_" + x.lower() for x in proc_list]
     # proc_list = ["p_itf_" + x.lower() for x in proc_list]
-    error_list = batch_replace(proc_list, "view_and_log")
-    print("-------------", error_list)
+    proc_list = [
+        "p_rpt_cif058",
+        "p_rpt_cif150",
+        "p_rpt_cif613_3",
+        "p_rpt_cifsb013",
+        "p_rpt_cif088e",
+        "p_rpt_cif088k",
+        "p_rpt_cif088rc",
+        "p_rpt_cif089_1",
+        "p_rpt_cif089c1",
+        "p_rpt_cif089c2",
+        "p_rpt_cif089cn",
+        "p_rpt_cif089cp",
+        "p_rpt_cif089ka",
+        "p_rpt_cif089kn",
+        "p_rpt_cif089kp",
+        "p_rpt_cif089r1",
+        "p_rpt_cif089r2",
+        "p_rpt_cif089rn",
+        "p_rpt_cif089rp",
+        "p_rpt_cif096",
+    ]
+    error_dict = batch_replace(proc_list, "view_and_log")
+    print("-------------", error_dict)
