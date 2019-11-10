7f8707ba18462f5c9d3e7e5dc7e3a7427d7a2b00
klgentle <klgentle@sina.com>
2019-11-10 19:15:11 +0800

    add

---
+++ tests/unittest_demo.py
@@ -0,0 +1,14 @@
+class TestDict(unittest.TestCase):
+
+    def setUp(self):
+        print('setUp...')
+
+    def test_some_case(self):
+        pass
+
+    def tearDown(self):
+        print('tearDown...')
+
+
+if __name__ == '__main__':
+    unittest.main()
