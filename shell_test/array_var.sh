658545272e4fd6445743f543f39030e9135af070
klgentle <klgentle@sina.com>
2020-01-22 21:16:12 +0800

    shell test update

---
+++ shell_test/array_var.sh
@@ -0,0 +1,11 @@
+array_var[0]="test1"
+array_var[1]="test2"
+array_var[2]="test3"
+array_var[3]="test4"
+array_var[4]="test5"
+array_var[5]="test6"
+
+echo ${array_var[0]}
+index=5
+echo ${array_var[$index]}
+echo ${array_var[*]}
