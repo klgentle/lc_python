7d1e971eb81d101fc354f4a7e8c80ab1facab291
klgentle <klgentle@sina.com>
2020-03-28 23:21:23 +0800

    subprogress is fastercd ../!

--- scrapy_test/nsfwpicx/html_download_picture.py
+++ scrapy_test/nsfwpicx/html_download_picture.py
@@ -2,9 +2,11 @@ import requests
 import os
 import re
 import random
-#import urllib.request
+
+import urllib.request
 import time
 import pprint
+import subprocess
 
 from bs4 import BeautifulSoup
 from urllib.parse import quote_plus, unquote_plus
@@ -16,14 +18,14 @@ class GetNsfwPicture(object):
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"
         }
         self.root = "/mnt/d/code_test/game_test/nsf"
-    
-    @staticmethod
-    def find_read_number(input_str)-> int:
-        # 删除非数字(-)的字符串
-        return int(re.sub(r'\D', "", input_str))
 
-    #@staticmethod
-    #def change_char_to_number(filename)-> str:
+    @staticmethod
+    def find_read_number(input_str) -> int:
+        # 删除非数字(-)的字符串
+        return int(re.sub(r"\D", "", input_str))
+
+    # @staticmethod
+    # def change_char_to_number(filename)-> str:
     #    # xxx.jpg to ord(x)ord(x)ord(x).jpg
     #    name_list = filename.split(".")
     #    # 尽量不要用临时变量，容易误导人
@@ -54,7 +56,7 @@ class GetNsfwPicture(object):
             print("read_number", read_number)
 
             # 可能有坑，尤其是新页面阅读量少
-            #if read_number < 2000:
+            # if read_number < 2000:
             #    print("Read_number to small", pageNumber)
             #    return (" ", None)
 
@@ -69,7 +71,7 @@ class GetNsfwPicture(object):
         except Exception as e:
             print("保存图片链接失败:", e.__doc__)
         picList = sorted(list(set(picList)))
-        #pprint.pprint(picList)
+        # pprint.pprint(picList)
 
         return (title, picList)
 
@@ -81,24 +83,27 @@ class GetNsfwPicture(object):
             os.mkdir(root2)
 
         print("folder:", root2)
+        # 切换目录
+        os.chdir(root2)
         for i, addr in enumerate(picList):
             # 解决 windows 不区分大小写的问题
-            filename = addr.split("/")[-1] + str(i)
-            #filename = self.change_char_to_number(filename)
-            #datetime.datetime.now().microsecond
+            filename = "".join([str(i), "_", addr.split("/")[-1]])
             file_path = os.path.join(root2, filename)
+
             if not os.path.exists(file_path):
                 # save file
-                r = requests.get(addr, headers=self.kv)
-                r.raise_for_status()
-                with open(file_path, "wb") as f:
-                    f.write(r.content)
+                #r = requests.get(addr, headers=self.kv)
+                #r.raise_for_status()
+                #with open(file_path, "wb") as f:
+                #    f.write(r.content)
                 #urllib.request.urlretrieve(addr,filename=file_path)
+                # 多进程下载图片
+                subprocess.Popen(['curl', addr, '-o', file_path])
+
                 print("图片已保存")
             else:
                 print("-------------------图片已存在")
 
-    
     def download_one_html(self, pageNumber):
         titlelist = self.get_picure_addrs(pageNumber)
         self.download_picture(titlelist, pageNumber)
@@ -114,16 +119,9 @@ class GetNsfwPicture(object):
 
 if __name__ == "__main__":
     g = GetNsfwPicture()
-    #pageNumber = 1162
-    #g.download_one_html(pageNumber)
+    # pageNumber = 1162
+    # g.download_one_html(pageNumber)
 
-    from_number = 998 
-    end_number = 500 
+    from_number = 995
+    end_number = 500
     g.download_all_pictures(from_number, end_number)
-
-    #s = "sqbc.jpg"
-    #print(s)
-    #print(g.change_char_to_number(s))
-    #s = "SQBC.jpg"
-    #print(s)
-    #print(g.change_char_to_number(s))
---
+++ scrapy_test/nsfwpicx/test_subprogress.py
@@ -0,0 +1,5 @@
+import subprocess
+import os
+
+os.chdir("/mnt/d/code_test/game_test/nsf")
+subprocess.Popen(['curl', 'https://qpix.com/images/2020/03/27/svpd.jpg', '-o', '0_svpd.jpg'])
