ac202969e65b8a761b40f67b8ad2c07fe0572aac
klgentle <klgentle@sina.com>
2020-04-03 16:46:58 +0800

    update

--- automatic_office/CreateWeekReport.py
+++ automatic_office/CreateWeekReport.py
@@ -12,6 +12,7 @@ class CreateWeekReport(object):
     
     def get_all_weekday_interval(self, weekday_list:list) -> list :
         """
+        返回每个时间区间构成的列表,如 [(date'2020-03-07',date'2020-03-07')]
         weekday_list:[datetime.date(2020, 3, 5), datetime.date(2020, 3, 6),datetime.date(2020, 3, 7),datetime.date(2020, 3, 10),datetime.date(2020, 3, 13),datetime.date(2020, 3, 14)]
         
         """
@@ -20,16 +21,28 @@ class CreateWeekReport(object):
         interval_end = None
         interval = []
         for date in weekday_list:
-            if pre_date = None:
+            if pre_date is None:
                 interval_start = date        
-            elif date = pre_date + datetime.timedelta(1)
-                pass
+            elif date == pre_date + datetime.timedelta(1):
+                interval_end = date
+            else:
+                interval.append((interval_start, interval_end))
+                interval_start = date        
+                interval_end = date
+                
             pre_date = date
             
+        print("result:", interval)
+        return interval
 
 
 
 if __name__ == "__main__":
     obj = CreateWeekReport()
+    weekday_list1 = [datetime.date(2020, 3, 5), datetime.date(2020, 3, 6),datetime.date(2020, 3, 7)]
+    print(weekday_list1)
+    assert obj.get_all_weekday_interval(weekday_list1) == [(datetime.date(2020, 3, 5),datetime.date(2020, 3, 7))]
+
     weekday_list = [datetime.date(2020, 3, 5), datetime.date(2020, 3, 6),datetime.date(2020, 3, 7),datetime.date(2020, 3, 10),datetime.date(2020, 3, 13),datetime.date(2020, 3, 14)]
-    assert obj.get_all_weekday_interval(weekday_list) = [(datetime.date(2020, 3, 5),datetime.date(2020, 3, 7)),(datetime.date(2020, 3, 10),datetime.date(2020, 3, 10)), (datetime.date(2020, 3, 13), datetime.date(2020, 3, 14))]
+    print(weekday_list)
+    assert obj.get_all_weekday_interval(weekday_list) == [(datetime.date(2020, 3, 5),datetime.date(2020, 3, 7)),(datetime.date(2020, 3, 10),datetime.date(2020, 3, 10)), (datetime.date(2020, 3, 13), datetime.date(2020, 3, 14))]
