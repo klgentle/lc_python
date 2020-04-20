0673fa94e1627728cb57ec38917dd6272cf401cb
klgentle <klgentle@sina.com>
2020-04-20 21:13:16 +0800

    check logic

--- scrapy_test/nsfwpicx/html_download_picture.py
+++ scrapy_test/nsfwpicx/html_download_picture.py
@@ -24,19 +24,17 @@ class GetNsfwPicture(object):
         # 删除非数字(-)的字符串
         return int(re.sub(r"\D", "", input_str))
 
-    def get_picure_addrs(self, pageNumber):
+    def get_picure_addrs(self, url):
         """
         获取单个页面中的全部图片地址
         """
-        #url = "http://nsfwpicx.com/{}.html#".format(pageNumber)
-        #url = "http://nsfwpicx.com/2020/04/11/{}.html".format(pageNumber)
-        url = "http://nsfwpicx.com/2020/04/14/1333.html"
+        # url = "http://nsfwpicx.com/{}.html#".format(pageNumber)
+        # url = "http://nsfwpicx.com/2020/04/11/{}.html".format(pageNumber)
+        pageNumber = url.replace(".html", "").split("/")[-1]
+        print(f"test pageNumber: {pageNumber}")
+
         picList = []
         title = "_"
-        # try:
-        #    pass
-        # except Exception as e:
-        #    print("保存图片链接失败:", e.__doc__)
 
         r = requests.get(url, headers=self.kv)
         r.raise_for_status()
@@ -56,45 +54,43 @@ class GetNsfwPicture(object):
         #    return (" ", None)
 
         # 精确定位地址集
-        # picture_text = soup.find_all(attrs={"itemprop": "articleBody"})
-        # pprint.pprint("-------------picture_text",picture_text)
-
-        # pprint.pprint(r.text)
-        # p = re.compile("https://qpix.com/images/\d{4}/\d{2}/\d{2}/\w*\.jpg")
+        #http://45.147.200.153/images/2020/04/20/PG8T.jpg#vwid=860&vhei=1530
         p = re.compile(
-            "(https://qpix.com/images/\d{4}/\d{2}/\d{2}/\w*\.jpg|http://85.117.234.129/images/\d{4}/\d{2}/\d{2}/\w*\.jpg)"
+            "(https://qpix.com/images/\d{4}/\d{2}/\d{2}/\w*\.jpg|http://\d*.\d*.\d*.\d*/images/\d{4}/\d{2}/\d{2}/\w*\.jpg)"
         )
-        # tempList = re.findall(p, str(picture_text))
         tempList = re.findall(p, r.text)
         for addr in tempList:
             picList.append(addr)
-            # print("-------------test each addr:", addr)
         if len(picList) > 0:
             print("保存图片链接成功", pageNumber)
+        else:
+            print("保存图片链接失败", pageNumber)
+            pprint.pprint(r.text)
+            
         tempList = []
         pprint.pprint(list(set(picList)))
+        print("Picture number is:", len(set(picList)))
 
-        return (title, list(set(picList)))
+        return (title, pageNumber, list(set(picList)))
 
-    def download_picture(self, title_picList, pageNumber):
+    def download_picture(self, title_picList):
         # root 保存目录
         start = time.time()
-        title, picList = title_picList
+        title, pageNumber, picList = title_picList
         root2 = os.path.join(self.root, str(pageNumber) + "_" + title.replace(" ", "_"))
         if not os.path.exists(root2):
             os.mkdir(root2)
+        print("保存目录为：", root2)
 
         print("folder:", root2)
-        # 切换目录
-        # os.chdir(root2)
         for i, addr in enumerate(picList):
             # 解决 windows 不区分大小写的问题
             filename = "".join([str(i), "_", addr.split("/")[-1]])
             file_path = os.path.join(root2, filename)
 
             # 多进程下载图片
-            subprocess.Popen(['curl', addr, '-o', file_path, '--silent'])
-            print("图片下载中", pageNumber)
+            subprocess.Popen(["curl", addr, "-o", file_path, "--silent"])
+            print("图片下载中", i)
 
         # if not os.path.exists(file_path):
         #    pass
@@ -109,12 +105,12 @@ class GetNsfwPicture(object):
         # else:
         #    print("-------------------图片已存在")
 
-    def download_one_html(self, pageNumber):
-        titlelist = self.get_picure_addrs(pageNumber)
-        self.download_picture(titlelist, pageNumber)
+    def download_one_html(self, url):
+        pic_list = self.get_picure_addrs(url)
+        self.download_picture(pic_list)
 
     def download_all_pictures(self, from_number, end_number):
-        for i in range(from_number, end_number-1, -1):
+        for i in range(from_number, end_number - 1, -1):
             print("Deal with page index:", i)
             self.download_one_html(i)
             # 休息，防止被封, 等并行的下载完成
@@ -133,15 +129,9 @@ class GetNsfwPicture(object):
             time.sleep(sleep_second)
             print("---------Sleep {} second end---------\n".format(sleep_second))
 
+
 if __name__ == "__main__":
     g = GetNsfwPicture()
-    #pageNumber = 881
-    #g.download_one_html(pageNumber)
-
-    #from_number = 1289
-    #end_number = 1289
-    ##end_number = 1200
-    #g.download_all_pictures(from_number, end_number)
-
-    index_list = [1333]
-    g.download_list_pictures(index_list)
+    # g.download_list_pictures(index_list)
+    url = "http://nsfwpicx.com/2020/04/06/1285.html"
+    g.download_one_html(url)
