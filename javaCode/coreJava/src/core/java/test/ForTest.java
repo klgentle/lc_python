9d182d0ed31c7d7f82e245e2f8d50bf62433afc1
klgentle <klgentle@sina.com>
2020-06-17 10:03:03 +0800

    add new java file

---
+++ javaCode/coreJava/src/core/java/test/ForTest.java
@@ -0,0 +1,11 @@
+package core.java.test;
+
+public class ForTest {
+    public static void main(String[] args)
+    {
+        for ( int i = 10 ; i > 0 ; i--)
+            System.out.println("Counting down " + i ) ;
+        System.out.println("Blastoff ! ");
+    }
+
+}
