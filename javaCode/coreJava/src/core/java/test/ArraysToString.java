a0af59201ddc78feae8836785fd1f3470626bfd9
klgentle <klgentle@sina.com>
2020-06-18 08:23:22 +0800

    add

---
+++ javaCode/coreJava/src/core/java/test/ArraysToString.java
@@ -0,0 +1,11 @@
+package core.java.test;
+
+import java.util.Arrays;
+
+public class ArraysToString {
+    public static void main(String[] args)
+    {
+        int[] a = {1,2,3,6,8,9};
+        System.out.println(Arrays.toString(a));
+    }
+}
