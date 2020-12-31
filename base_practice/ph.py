a6112f43b151e10d2170d0b94b4d161495228968
klgentle <klgentle@sina.com>
2020-12-31 08:57:38 +0800

    test the use of pretty_errors

--- base_practice/ph.py
+++ base_practice/ph.py
@@ -1,7 +1,11 @@
-#ph=float()
+print(f"please input ph value:")
+ph=float(input())
+print(f"input:{ph}")
 if ph<7.0:
    neutral=0;base=0;acid=1
 elif ph>7.0:
    neutral=0;base=1;acid=0
 elif ph==7.0:
    neutral=1;base=0;acid=0
+
+print(f"neutral={neutral};base={base};acid={acid}")
