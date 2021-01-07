819eb9626f81cfde3824295e9ca71bf89fbfdda2
klgentle <klgentle@sina.com>
2021-01-07 08:06:53 +0800

    add answer for 322

--- leet_code/labuladong/dynamic_programming/P0322_Coin_Change.py
+++ leet_code/labuladong/dynamic_programming/P0322_Coin_Change.py
@@ -1,5 +1,5 @@
 """
-322. Coin Change TODO
+322. Coin Change TODO enhance
 Medium
 
 You are given coins of different denominations and a total amount of money amount. Write a function to compute the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
@@ -20,3 +20,25 @@ Input: coins = [2], amount = 3
 Output: -1
 
 """
+
+class Solution:
+    def coinChange(self, coins, amount:int) -> int:
+        #coins:List[int]
+        res = {}
+        res[0] = 0
+        
+        def dp(n):
+            # dp(n) = min(dp(n-coin)+1, dp(n))
+            if n in res.keys():
+                return res[n]
+            if n < 0:
+                return -1
+            res[n] = float("INF")
+            for coin in coins:
+                if dp(n-coin) == -1:
+                    continue             
+                res[n] = min(dp(n-coin) + 1, res[n])
+                
+            return res[n] if res[n] != float("INF") else -1
+
+        return dp(amount)
