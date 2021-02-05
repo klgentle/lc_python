9346fdebb1019b7a3a3debc35fdd867e95a45783
klgentle <klgentle@sina.com>
2021-02-05 09:38:03 +0800

    update add 直接计算的方法

--- leet_code/labuladong/tree/p0222_Count_Complete_Tree_Nodes.py
+++ leet_code/labuladong/tree/p0222_Count_Complete_Tree_Nodes.py
@@ -26,8 +26,28 @@ Output: 1
 #         self.left = left
 #         self.right = right
 class Solution:
-    def countNodes(self, root: TreeNode) -> int:
+    def countNodes1(self, root: TreeNode) -> int:
         if not root:
             return 0
 
+        return 1+ self.countNodes(root.left) + self.countNodes(root.right)
+
+    def countNodes(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        
+        l = root.left
+        r = root.right
+        
+        hl, hr = 0, 0
+        while l:
+            l = l.left
+            hl += 1
+        while r:
+            r = r.right
+            hr += 1
+        
+        if hl == hr:
+            return 2 ** (hl+1) -1
+        
         return 1+ self.countNodes(root.left) + self.countNodes(root.right)
