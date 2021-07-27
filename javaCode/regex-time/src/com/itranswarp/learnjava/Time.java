1108c1b73b257f10ffd03514d1ecda26c638a7dc
klgentle <klgentle@sina.com>
2021-07-27 17:57:19 +0800

    update

--- javaCode/regex-time/src/com/itranswarp/learnjava/Time.java
+++ javaCode/regex-time/src/com/itranswarp/learnjava/Time.java
@@ -14,30 +14,19 @@ public class Time {
 	 * 从"21:59:59"中提取时，分，秒，否则抛出IllegalArgumentException
 	 */
 	public static int[] parseTime(String s) {
-		// FIXME:
-		int[] res = new int[3];
-		Pattern pattern = Pattern.compile("([0-2]\\d):([0-5]\\d):([0-5]\\d)");
-		if (s == null || s.length() < 8 || s.substring(0, 2).equals("24") || s.substring(3, 5).equals("60")
-				|| s.substring(6, 8).equals("60")) {
+		if (s == null || s.length() < 8) {
 			throw new IllegalArgumentException();
 		}
 
-		try {
-			Matcher matcher = pattern.matcher(s);
-			if (matcher.matches()) {
-//				String group0 = matcher.group(0);  // 0表示匹配的整个字符串
-				String group1 = matcher.group(1);
-				String group2 = matcher.group(2);
-				String group3 = matcher.group(3);
-//				System.out.println(group1);
-				res[0] = group1.charAt(0) == '0' ? Integer.valueOf(group1.substring(1)) : Integer.valueOf(group1);
-				res[1] = group2.charAt(0) == '0' ? Integer.valueOf(group2.substring(1)) : Integer.valueOf(group2);
-				res[2] = group3.charAt(0) == '0' ? Integer.valueOf(group3.substring(1)) : Integer.valueOf(group3);
-			}
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
+		Pattern pattern = Pattern.compile("([0-1]\\d|[0-2][0-3]):([0-5]\\d):([0-5]\\d)");
+		Matcher matcher = pattern.matcher(s);
+
+		if (matcher.matches()) {
+			return new int[] { Integer.valueOf(matcher.group(1)), Integer.valueOf(matcher.group(2)),
+					Integer.valueOf(matcher.group(3)) };
+		} else {
+			throw new IllegalArgumentException();
 		}
-		return res;
 	}
 
 }
---
+++ javaCode/regex/Main3.java
@@ -0,0 +1,7 @@
+public class Main3 {
+    public static void main(String[] args) {
+        String s1 = "01";
+        int p = Integer.valueOf(s1);
+        System.out.println(p);
+    }
+}
