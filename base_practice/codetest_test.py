7a85564259348b307eb4f843fb3e58fcfb5873ab
klgentle <klgentle@sina.com>
2022-07-31 11:15:52 +0800

    add files to use codetest

---
+++ base_practice/codetest_test.py
@@ -0,0 +1,14 @@
+def print_factors(n):
+    """Print the prime factors of N.
+    >>> print_factors(180)
+    2
+    2
+    3
+    5
+    """
+    pass
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
